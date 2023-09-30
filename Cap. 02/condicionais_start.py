fc5b40bebabb923701ea5377ce22820496f36ba5
mnqrs <116473798+mnqrs@users.noreply.github.com>
2023-09-30 15:09:24 -0300

    atividades

---
+++ Cap. 02/condicionais_start.py
@@ -0,0 +1,16 @@
+#
+# Arquivo de exemplo das estruturas condicionais
+#
+
+def condicionais():
+    x, y = 1010, 1010
+
+    if (x < y):
+        print("x é menor do que y")
+    elif (x == y):
+        print("x é igual a y")    
+    else:
+        print("x é maior do que y")
+            
+
+condicionais()
